cd283cba4e466fcb3852ded647c0aa0c29674133
Daniel Potts <drp777@hotmail.com>
2018-06-28 15:25:58 -0500

    first push

---
+++ assets/javascript/app.js
@@ -0,0 +1,18 @@
+// create an array of cities, new cities will be pushed into this array;
+//loop throught the array 
+// run a function that will create the gif buttons
+// .empty will erase anything in the div to prevent duplication
+// we'll need a function to create a new action button and form method to create the input place
+// we'll need to link to the gif api as well to 
+
+var cities = [Chicago, Detroit, San Francisco, Panama City, Sevilla, Tangier, Barcelona, Paris];
+
+    //for loop through the cities and create the buttons//
+
+    for(i = 0; i < cities.length; i++) {
+        console.log(cities);
+        
+    }
+
+
+
