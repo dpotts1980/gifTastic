4c2c7c1c91d4d44a8e619ae518e30505b7c19192
Daniel Potts <drp777@hotmail.com>
2018-06-29 15:03:26 -0500

    y

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,24 +1,58 @@
 // create an array of cities, new cities will be pushed into this array;
-//loop throught the array 
+//loop through the array 
 // run a function that will create the gif buttons
 // .empty will erase anything in the div to prevent duplication
-// we'll need a function to create a new action button and form method to create the input place
+// we'll need a function to create a new city button and form method to create the input place
 // we'll need to link to the gif api as well to 
 
-var cities = ["Chicago", "Detroit", "San Francisco", "Panama City", "Sevilla", "Tangier", "Barcelona", "Paris"];
+var citiesArray = ["Chicago", "Detroit", "San Francisco", "Panama City", "Sevilla", "Tangier", "Barcelona", "Paris"];
 
-  
+    
+    
     function displayButtons(){
         $("#cityButtonsDisplayed").empty();
     //for loop through the cities and create the buttons//
-    for(i = 0; i < cities.length; i++) {
-        var cityButtons =  $("<button>");
-        console.log(cityButtons);
+    for(i = 0; i < citiesArray.length; i++) {
+        console.log(citiesArray[i].length);
+        var cityButtons =  $("<button>" + citiesArray[i] + "</button>");
+        cityButtons.addClass("action");
+        cityButtons.addClass("btn btn-info search-giphy");
+        var cityButtonsDisplayed = $('#cityButtonsDisplayed');
+        cityButtonsDisplayed.append(cityButtons);
+        console.log(cityButtonsDisplayed);
         
-
-        
-    };
-
+        };
+    
 
 
     }
+    displayButtons();
+
+    $(document).on("click", ".search-giphy", searchGiphy);
+    
+    function searchGiphy(){
+        console.log('hi')
+        var cityData= $(this).text();
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + cityData + "&api_key=wb7VXuuCp9IduDjRrvhZZ2RNrAA1U1KV";
+        $.ajax({
+            url: queryURL,
+            type: "GET"
+          }).then(function(response){
+
+            var url = response.data[0].images.fixed_height.url
+          })
+    }
+
+      /*
+     $("button").on("click", function() {
+      var person = $(this).attr("data-person");
+      var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+        person + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+      $.ajax({
+        url: queryURL,
+        method: "GET"
+      })
+        .then(function(response) {
+          var results = response.data
+    */
